cc08188d2ed3e0dbfc301464f0d09bda8f2a1f6b
youssefali424 <youssefali4242014@gmail.com>
2020-02-07 21:34:26 +0200

    format

--- src/async-sequence/elementAtOrNull.ts
+++ src/async-sequence/elementAtOrNull.ts
@@ -1,23 +1,24 @@
 import { AsyncSequence } from "./asyncSequence";
 
 export class ElementAtOrNull {
-
-    /**
-     * Returns the element at position `index` (zero-based) or `null` if `index`
-     * is out of bounds.
-     *
-     * @param {number} index
-     * @returns {Promise<T | null>}
-     */
-    async elementAtOrNull<T>(this: AsyncSequence<T>, index: number): Promise<T | null>{
-        let i = 0;
-        for await (let item of this.iterator) {
-            if (i === index) {
-                return item;
-            }
-            i++;
-        }
-        return null;
+  /**
+   * Returns the element at position `index` (zero-based) or `null` if `index`
+   * is out of bounds.
+   *
+   * @param {number} index
+   * @returns {Promise<T | null>}
+   */
+  async elementAtOrNull<T>(
+    this: AsyncSequence<T>,
+    index: number
+  ): Promise<T | null> {
+    let i = 0;
+    for await (let item of this.iterator) {
+      if (i === index) {
+        return item;
+      }
+      i++;
     }
-
+    return null;
+  }
 }
--- src/async-sequence/firstOrNull.ts
+++ src/async-sequence/firstOrNull.ts
@@ -1,31 +1,33 @@
 import { AsyncSequence } from "./asyncSequence";
 
 export class FirstOrNull {
-
-    /**
-     * Returns the first element of the sequence or the first element matching `predicate` if present, otherwise returns `null`.
-     *
-     * @param {(T) => boolean} predicate
-     * @returns {T}
-     */
-    async firstOrNull<T>(this: AsyncSequence<T>, predicate?: (item: T) => boolean): Promise<T | null> {
-        if (predicate != null) {
-            return this.filter(predicate).firstOrNull();
-        }
-        const item = await this.iterator.next();
-        return item.done
-            ? null
-            : item.value;
+  /**
+   * Returns the first element of the sequence or the first element matching `predicate` if present, otherwise returns `null`.
+   *
+   * @param {(T) => boolean} predicate
+   * @returns {T}
+   */
+  async firstOrNull<T>(
+    this: AsyncSequence<T>,
+    predicate?: (item: T) => boolean
+  ): Promise<T | null> {
+    if (predicate != null) {
+      return this.filter(predicate).firstOrNull();
     }
+    const item = await this.iterator.next();
+    return item.done ? null : item.value;
+  }
 
-    /**
-     * Returns the first element of the sequence or the first element matching `predicate` if present, otherwise returns `null`.
-     *
-     * @param {(T) => boolean} predicate
-     * @returns {Promise<T | null>}
-     */
-    async find<T>(this: AsyncSequence<T>, predicate?: (item: T) => boolean): Promise<T | null>{
-        return this.firstOrNull(predicate);
-    }
-
+  /**
+   * Returns the first element of the sequence or the first element matching `predicate` if present, otherwise returns `null`.
+   *
+   * @param {(T) => boolean} predicate
+   * @returns {Promise<T | null>}
+   */
+  async find<T>(
+    this: AsyncSequence<T>,
+    predicate?: (item: T) => boolean
+  ): Promise<T | null> {
+    return this.firstOrNull(predicate);
+  }
 }
